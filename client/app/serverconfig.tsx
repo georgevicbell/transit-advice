2ef3d8b9d4bcd419f21fb4c029ae3f04bd6ea3ae
George Bell <4380159+georgevicbell@users.noreply.github.com>
2025-02-22 15:40:34 -0800

    update config screen with country, state

--- client/app/serverconfig.tsx
+++ client/app/serverconfig.tsx
@@ -1,21 +1,25 @@
 
 import Slider from '@react-native-community/slider';
-import { Picker } from '@react-native-picker/picker';
 import { StatusBar } from 'expo-status-bar';
 import { useContext, useEffect, useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { DataContext } from '../src/context/DataContext';
 import { useStyleContext } from '../src/context/StyleContext';
 import { AgencyItem, RouteItem } from '../src/context/types';
-
+import { unique } from '../src/Utils';
 export default function Page() {
     const styles = useStyleContext();
     if (!styles) return null
+    const [country, setCountry] = useState<string>()
+    const [state, setState] = useState<string>()
     const [agency, setAgency] = useState<AgencyItem>()
     const [route, setRoute] = useState<RouteItem>()
     const [bufferWidth, setBufferWidth] = useState<number>(0.1)
     const { config, agencyList, routeList, saveConfig, saveData, setTempAgency } = useContext(DataContext)
     useEffect(() => {
+        setState(config?.agency?.state)
+        setCountry(config?.agency?.country)
         setAgency(config?.agency)
         setRoute(config?.route)
         setBufferWidth(config?.bufferWidth ?? 0.1)
@@ -23,57 +27,95 @@ export default function Page() {
     useEffect(() => {
         setTempAgency(agency)
     }, [agency])
+    console.log(agencyList)
+    console.log(route)
     return (
-        <View style={{ flex: 1 }}>
-            <View>
-                <Text>Agency: </Text>
-                <Picker
-                    selectedValue={agency?.id}
-                    onValueChange={(itemValue, itemIndex) => {
-                        const item = agencyList.filter(z => { return z.id == itemValue })[0]
-                        console.log(item)
-                        setAgency(item)
-                    }
-                    }>
-                    {agencyList?.map((z: any) => { return <Picker.Item key={z.id} label={z.title} value={z.id} /> })}
+        <View style={{ flex: 1, margin: 10, padding: 10 }}>
+            <Text style={{ fontWeight: "bold" }}>Setup Route:</Text>
+            <View style={{ flexDirection: "row", padding: 10, margin: 10 }}>
 
-                </Picker>
-                <Text>Route:</Text>
-                <Picker
-                    selectedValue={route?.id}
-                    onValueChange={(itemValue, itemIndex) => {
-                        const item = routeList.filter(z => { return z.id == itemValue })[0]
-                        console.log(item)
-                        setRoute(item)
-                    }
-                    }>
-                    {routeList?.map((z: any) => { return <Picker.Item key={z.id} label={z.title} value={z.id} /> })}
+                <View style={{ borderWidth: 1 }}>
+                    <Text style={{ padding: 10, fontWeight: "bold" }}>Country </Text>
+                    <ScrollView style={{ height: 200 }}>
+                        {
+                            unique(agencyList, "country").map((countryS, index) => {
+                                return <Pressable key={index} style={{ padding: 10, backgroundColor: country == countryS.country ? "lightgray" : undefined }} onPress={() => {
+                                    setCountry(countryS.country)
+                                    setState(undefined)
+                                    setAgency(undefined)
+                                    setRoute(undefined)
+                                }}><Text>{countryS.country}</Text></Pressable>
+                            })
+                        }
+                    </ScrollView>
+                </View>
+                {country && <View style={{ borderWidth: 1 }}>
+                    <Text style={{ fontWeight: "bold", padding: 10 }}>Region </Text>
+                    <ScrollView style={{ height: 200 }}>
+                        {
+                            unique(agencyList.filter(z => z.country == country), "state").map((stateS, index) => {
+                                return <Pressable key={index} style={{ padding: 10, backgroundColor: state == stateS.state ? "lightgray" : undefined }} onPress={() => {
+                                    setState(stateS.state)
 
-                </Picker>
-                <Text>On Route Buffer Width: {(bufferWidth ?? 0.05) * 1000} meters</Text>
-
-                <Slider
-                    style={{ width: 200, height: 40 }}
-                    minimumValue={0.01}
-                    maximumValue={1}
-                    minimumTrackTintColor="#FFFFFF"
-                    maximumTrackTintColor="#000000"
-                    value={bufferWidth}
-                    step={0.01}
-                    onValueChange={(value) => {
-                        setBufferWidth(value)
-                    }}
-                />
-
-                <Pressable onPress={() => {
-                    saveConfig({ agency, route, bufferWidth })
-                }}>
-                    <Text>Save Config</Text>
-                </Pressable>
-                <Pressable onPress={saveData}>
-                    <Text>Save Data</Text>
-                </Pressable>
+                                    setAgency(undefined)
+                                    setRoute(undefined)
+                                }}><Text>{stateS.state}</Text></Pressable>
+                            })
+                        }
+                    </ScrollView>
+                </View>}
+                {country && state && <View style={{ borderWidth: 1 }}>
+                    <Text style={{ fontWeight: "bold", padding: 10 }}>Agency </Text>
+                    <ScrollView style={{ height: 200 }}>
+                        {
+                            agencyList.filter(z => z.country == country).filter(z => z.state == state).map((agencyS, index) => {
+                                return <Pressable key={index} style={{ padding: 10, backgroundColor: agency?.id == agencyS.id ? "lightgray" : undefined }} onPress={() => {
+                                    setAgency(agencyS)
+                                    setRoute(undefined)
+                                }}><Text>{agencyS.title}</Text></Pressable>
+                            })
+                        }
+                    </ScrollView>
+                </View>}
+                {agency && <View style={{ borderWidth: 1 }}>
+                    <Text style={{ fontWeight: "bold", padding: 10 }}>Route </Text>
+                    <ScrollView style={{ height: 200 }}>
+                        {
+                            routeList?.map((routeS, index) => {
+                                return <Pressable key={index} style={{ padding: 10, backgroundColor: routeS.id == route?.id ? "lightgray" : undefined }} onPress={() => {
+                                    setRoute(routeS)
+                                }}><Text>{routeS.title}</Text></Pressable>
+                            })
+                        }
+                    </ScrollView>
+                </View>}
             </View>
+
+
+            <Text>On Route Buffer Width: {(bufferWidth ?? 0.05) * 1000} meters</Text>
+
+            <Slider
+                style={{ width: 200, height: 40 }}
+                minimumValue={0.01}
+                maximumValue={1}
+                minimumTrackTintColor="#FFFFFF"
+                maximumTrackTintColor="#000000"
+                value={bufferWidth}
+                step={0.01}
+                onValueChange={(value) => {
+                    setBufferWidth(value)
+                }}
+            />
+
+            <Pressable onPress={() => {
+                saveConfig({ agency, route, bufferWidth })
+            }}>
+                <Text>Save Config</Text>
+            </Pressable>
+            <Pressable onPress={saveData}>
+                <Text>Save Data</Text>
+            </Pressable>
+
 
             <StatusBar style="auto" />
         </View >
